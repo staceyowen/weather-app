19d4dfda22a5f07b522a3f1ac4700047a021ad47
staceyowen <70521619+staceyowen@users.noreply.github.com>
2020-10-12 18:54:44 +0100

    Removed console.log and notes

--- src/app.js
+++ src/app.js
@@ -19,13 +19,11 @@ function formateDate(timestamp) {
     ];
     let day = days[date.getDay()];
 
-// calculate the time and the date and return
 return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
-    console.log(response.data);
-    
+ 
     let cityElement = document.querySelector("#city");
     let temperatureElement = document.querySelector("#temperature");
     let descriptionElement = document.querySelector("#weather-description");
@@ -41,12 +39,11 @@ function displayTemperature(response) {
     windElement.innerHTML = Math.round (response.data.wind.speed);
     feelslikeElement.innerHTML = Math.round (response.data.main.feels_like);
     dateElement.innerHTML = formateDate(response.data.dt * 1000);
-    // *1000 to convert into milliseconds.
 
 }
 
 let apiKey = "dfc2e82cce5dfe54171afee9e327c236";
-  let city = "Los Angeles"
+  let city = "Tokyo"
   let unit = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
 
