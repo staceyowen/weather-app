710a9739761c7a7284b34b0ef7bb1877eb55c29b
staceyowen <70521619+staceyowen@users.noreply.github.com>
2020-10-15 16:34:29 +0100

    Temperature conversion

--- src/app.js
+++ src/app.js
@@ -33,8 +33,10 @@ function displayTemperature(response) {
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
 
+    celsiusTemperature = response.data.main.temp;
+
     cityElement.innerHTML = response.data.name;
-    temperatureElement.innerHTML = Math.round (response.data.main.temp);
+    temperatureElement.innerHTML = Math.round (celsiusTemperature);
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round (response.data.wind.speed);
@@ -58,8 +60,32 @@ function handleSubmit(event) {
     console.log(cityInputElement.value);
 }   
 
+function displayFahrenheitTemp(event) {
+    event.preventDefault();
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5+ 32;
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
 
-search("Tokyo");
+function displayCelsiusTemp(event) {
+    event.preventDefault();
+     celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+ celsiusLink.addEventListener("click", displayCelsiusTemp);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
+
+search("Tokyo");
