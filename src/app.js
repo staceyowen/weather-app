04c23ed4e805d13e0d91cc1117bfa4b71be67bfc
staceyowen <70521619+staceyowen@users.noreply.github.com>
2020-10-20 20:19:38 +0100

    Moved forecast from HTML to JS and using API to build the forecast

--- src/app.js
+++ src/app.js
@@ -22,6 +22,19 @@ function formateDate(timestamp) {
 return `${day} ${hours}:${minutes}`;
 }
 
+function formatHours(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+     if (hours < 10) {
+        hours = `0${hours}`;
+     }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    return `${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
  
     let cityElement = document.querySelector("#city");
@@ -46,11 +59,41 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function displayForecast(response) {
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = null;
+let forecast = null;
+
+for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+    <div class="col-2">
+        <h3>
+          ${formatHours(forecast.dt * 1000)}
+        </h3>
+        <img 
+        src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" 
+        alt="weather"
+        />
+        <div class="future-temperature">
+            <strong>
+                ${Math.round(forecast.main.temp_max)}°
+            </strong> 
+            ${Math.round(forecast.main.temp_min)}°
+        </div>
+    </div>
+`; 
+}
+}   
+
 function search(city) {
     let apiKey = "dfc2e82cce5dfe54171afee9e327c236";
     let unit = "metric";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
     axios.get(apiUrl).then(displayTemperature);
+
+    apiUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${unit}`;
+    axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
